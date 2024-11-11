89d7751c8b43640dd2c33719d66a4ae0543aec38
auxi-asz <jakedyl005@gmail.com>
2024-11-11 16:07:23 +1100

    basics being setup

--- processing.py
+++ processing.py
@@ -1,6 +1,17 @@
-import os, PIL
 from PIL import Image, ImageEnhance
+import os
 
 directory = r"C:\Users\calxo\Documents\Image-Modifier\textures"
+reset = r"C:\Users\calxo\Documents\Image-Modifier\base"
+
+print("Enter the factor for modifying the brightness of the files:")
+userinput = float(input())
 
 
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    file = os.path.join(filename)
+    if os.path.isfile(f):
+        print(file)
+    
